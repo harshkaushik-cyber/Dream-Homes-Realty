c70669f9a6958bd614001dc603a443ca6c854975
harshkaushik-cyber <kaushikharsh186@gmail.com>
2025-04-17 22:40:20 +0530

    final code

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelector('.nav-links');
+const navItems = document.querySelectorAll('.nav-links li a');
 
+// Toggle menu on burger click
 navToggle.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
+    navLinks.classList.toggle('active');
 });
+
+// Close menu when a link is clicked
+navItems.forEach(link => {
+    link.addEventListener('click', () => {
+        navLinks.classList.remove('active');
+    });
+});
+
